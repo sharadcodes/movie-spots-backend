75901958f1df2b2709bab97ebca814b147ecb552
Sharad Raj <36638057+sharadcodes@users.noreply.github.com>
2020-06-23 17:05:53 +0530

    Update migrate.js

--- data/migrate.js
+++ data/migrate.js
@@ -2,7 +2,7 @@ const mongodb = require("mongodb").MongoClient;
 const csvtojson = require("csvtojson");
 
 // let url = "mongodb://username:password@localhost:27017/";
-let url = "mongodb://localhost:27017/";
+let url = process.env.MONGODB_URI;
 
 csvtojson()
   .fromFile(__dirname + "/Film_Locations_in_San_Francisco.csv")
